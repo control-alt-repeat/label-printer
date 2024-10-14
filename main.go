96ce76a5ae87f81af39f4437676c99077c5b6dc7
Matt Canty <matt.canty@controlaltrepeat.net>
2024-10-14 13:56:34 +0100

    Delete from buffer after print without failure

--- main.go
+++ main.go
@@ -3,24 +3,42 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path/filepath"
+	"strings"
 	"syscall"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/ssm"
 
 	localtunnel "github.com/localtunnel/go-localtunnel"
 )
 
-func main() {
-	fmt.Print("Validating AWS credentials")
+type Printer struct {
+	Port string
+	Name string
+}
 
+var printerMap = map[string]Printer{
+	"62": {
+		Port: "usb://0x04f9:0x2015",
+		Name: "QL-500",
+	},
+	"102x152": {
+		Port: "usb://0x04f9:0x202a",
+		Name: "QL-500",
+	},
+}
+
+func main() {
 	aws_session, err := session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-2")},
 	)
@@ -38,9 +56,10 @@ func main() {
 	fmt.Printf("Tunnel URL: %s\n", tunnel.URL())
 
 	input := &ssm.PutParameterInput{
-		Name:  aws.String("/control_alt_repeat/ebay/live/label_printer/host_domain"),
-		Value: aws.String(tunnel.URL()),
-		Type:  aws.String("String"),
+		Name:      aws.String("/control_alt_repeat/ebay/live/label_printer/host_domain"),
+		Value:     aws.String(tunnel.URL()),
+		Type:      aws.String("String"),
+		Overwrite: aws.Bool(true),
 	}
 
 	ssm_svc := ssm.New(aws_session)
@@ -61,7 +80,7 @@ func main() {
 		Handler:      middleware{handlerMux},
 	}
 
-	handlerMux.HandleFunc("/hello", hello)
+	handlerMux.HandleFunc("/webhook", webhook)
 	handlerMux.HandleFunc("/ping", ping)
 	handlerMux.HandleFunc("/print-cat", printCat)
 
@@ -87,24 +106,59 @@ func main() {
 	fmt.Println("service has shutdown")
 }
 
-func hello(rw http.ResponseWriter, req *http.Request) {
-	u := req.Context().Value("user")
-	user := "unset"
-	if u != nil {
-		user = u.(string)
-	}
-
+func webhook(rw http.ResponseWriter, req *http.Request) {
 	switch req.Method {
-	case http.MethodGet:
-		if _, err := rw.Write([]byte("Hello " + user + "\n")); err != nil {
-			fmt.Println("error when writing response for /hello request")
-			rw.WriteHeader(http.StatusInternalServerError)
-		}
 	case http.MethodPost:
-		if _, err := rw.Write([]byte("Thanks for posting to me, " + user + "\n")); err != nil {
-			fmt.Println("error when writing response for /hello request")
-			rw.WriteHeader(http.StatusInternalServerError)
+		aws_session, err := session.NewSession(&aws.Config{
+			Region: aws.String("eu-west-2")},
+		)
+		if err != nil {
+			fmt.Println("Failed to create new session", err)
+			return
 		}
+
+		svc := s3.New(aws_session)
+		bucket := "control-alt-repeat-label-print-buffer"
+
+		result, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+			Bucket: aws.String(bucket),
+		})
+		if err != nil {
+			fmt.Println("Unable to list items in bucket", bucket, err)
+			return
+		}
+
+		for _, item := range result.Contents {
+			fmt.Printf("Downloading %s from bucket %s\n", *item.Key, bucket)
+
+			err := downloadFile(svc, bucket, *item.Key)
+			if err != nil {
+				fmt.Println("Unable to download item:", err)
+			}
+
+			format_name := strings.SplitN(*item.Key, "-", 2)[0]
+			printer := printerMap[format_name]
+
+			cmd := exec.Command("brother_ql",
+				"-b", "pyusb",
+				"-m", printer.Name,
+				"-p", printer.Port,
+				"print",
+				"-l", format_name,
+				*item.Key)
+
+			output, err := cmd.CombinedOutput()
+
+			if err != nil {
+				fmt.Printf("Command execution failed: %v", err)
+			} else {
+				deleteFile(svc, bucket, *item.Key)
+			}
+
+			fmt.Println(output)
+		}
+
+		rw.Write([]byte(""))
 	}
 }
 
@@ -152,4 +206,34 @@ func (m middleware) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	start := req.Context().Value("__requestStartTimer__").(time.Time)
 	fmt.Println("request duration: ", time.Since(start))
+}
+
+func downloadFile(svc *s3.S3, bucket, key string) error {
+	file, err := os.Create(filepath.Base(key))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	result, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(file, result.Body)
+
+	return err
+}
+
+func deleteFile(svc *s3.S3, bucket, key string) error {
+	_, err := svc.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	return err
 }
